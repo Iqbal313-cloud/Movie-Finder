013978bc5eb1af4ad72d68e12b3ae0f1ffc8b59e
Iqbal <muhammadiqbalxyz251@gmail.com>
2024-08-29 00:17:10 +0600

    after 116

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,5 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
     <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
   </StrictMode>
 );
