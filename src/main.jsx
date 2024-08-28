3aff14a6a05902ff02b752902902d3d2c0a76400
Iqbal <muhammadiqbalxyz251@gmail.com>
2024-08-28 23:38:52 +0600

    reuseable star

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import App from './App.jsx'
+// import './index.css'
+import StarRating from "./StarRating";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating />
+  </StrictMode>
+);
