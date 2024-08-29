8db92980b35b9f220540859553d6b545a83026d3
Iqbal <muhammadiqbalxyz251@gmail.com>
2024-08-29 20:35:48 +0600

    at the end of star component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,27 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import App from './App.jsx'
 // import './index.css'
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 import StarRating from "./StarRating";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "bad", "okay", "good", "amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </StrictMode>
 );
